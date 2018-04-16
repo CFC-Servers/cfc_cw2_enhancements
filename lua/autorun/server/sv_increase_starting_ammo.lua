0171085eccf809ead2cf79d6b3ca8b97db7b5c6e
Brandon Sturgeon <sturgeonb4@gmail.com>
2018-04-15 20:42:20 -0700

    Functional update

--- lua/autorun/server/sv_increase_starting_ammo.lua
+++ lua/autorun/server/sv_increase_starting_ammo.lua
@@ -1,18 +1,22 @@
 
 function giveCW2AmmoOnSpawn( player )
-    local cw2Weapons = {}
-    for _, weapon in pairs(player:GetWeapons()) do
-        local isCW2Weapon = string.sub(weapon:GetClass(), 1, 2) == "cw"
-        if ( isCW2Weapon ) then
-            table.insert( cw2Weapons, weapon)
-        end
-    end
+	timer.Simple(0, function()
+		local cw2Weapons = {}
+		for _, weapon in pairs(player:GetWeapons()) do
+			local isCW2Weapon = string.sub(weapon:GetClass(), 1, 2) == "cw"
+			if ( isCW2Weapon ) then
+				table.insert( cw2Weapons, weapon)
+			end
+		end
 
-    for _, cw2Weapon in pairs(cw2Weapons) do
-        local ammoType = cw2Weapon:GetPrimaryAmmoType()
-        player:GiveAmmo("90", ammoType, false)
-        cw2Weapon:SetClip1(31)
-    end
+		for _, cw2Weapon in pairs(cw2Weapons) do
+			local ammoType = cw2Weapon:GetPrimaryAmmoType()
+			local currentAmmo = player:GetAmmoCount( ammoType )
+			if ( currentAmmo == 0) then
+				player:GiveAmmo("90", ammoType, false)
+			end
+		end
+	end)
 end
 
 hook.Remove("PlayerSpawn", "CFC-Give-CW2-Ammo")
