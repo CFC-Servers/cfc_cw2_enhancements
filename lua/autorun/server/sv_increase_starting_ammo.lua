fc9d16c3a4f26af3041c1bb14b37c42adba33473
Brandon Sturgeon <sturgeonb4@gmail.com>
2018-04-19 11:19:05 -0700

    Simplify Ammo Script

--- lua/autorun/server/sv_increase_starting_ammo.lua
+++ lua/autorun/server/sv_increase_starting_ammo.lua
@@ -1,19 +1,18 @@
 
-function giveCW2AmmoOnSpawn( player )
+local function givePlayerAmmoForWeapon( player, weapon )
+	local ammoType = weapon:GetPrimaryAmmoType()
+	local currentAmmo = player:GetAmmoCount( ammoType )
+	if ( currentAmmo == 0 ) then
+		player:GiveAmmo("90", ammoType, false)
+	end
+end
+
+local function giveCW2AmmoOnSpawn( player )
 	timer.Simple(0, function()
-		local cw2Weapons = {}
 		for _, weapon in pairs(player:GetWeapons()) do
 			local isCW2Weapon = string.sub(weapon:GetClass(), 1, 2) == "cw"
 			if ( isCW2Weapon ) then
-				table.insert( cw2Weapons, weapon)
-			end
-		end
-
-		for _, cw2Weapon in pairs(cw2Weapons) do
-			local ammoType = cw2Weapon:GetPrimaryAmmoType()
-			local currentAmmo = player:GetAmmoCount( ammoType )
-			if ( currentAmmo == 0) then
-				player:GiveAmmo("90", ammoType, false)
+				givePlayerAmmoforWeapon( player, weapon )
 			end
 		end
 	end)
