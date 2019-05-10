9c47783935de95d2ac6c1c52ec2e132c7eb5e0e6
PLally <11771225+PLally@users.noreply.github.com>
2019-05-10 16:39:40 -0400

    prevent builders from being flashed

---
+++ lua/autorun/client/cw_flashbang_restrictions.lua
@@ -0,0 +1,33 @@
+local PLAYER = FindMetaTable("Player")
+
+local function wrapCwFlashbang() 
+    local flash = PLAYER.cwFlashbang
+    PLAYER.cwFlashbang = function(self, intensity, duration) 
+        local inPvp = ply:GetNWBool("CFC_PvP_Mode", false)
+        if !inPvp then return end
+        
+        return flash(self, intensity, duration)
+    end
+end
+
+local function waitingFor() 
+    return PLAYER.cwFlashbang ~= nil
+end
+
+local function onTimout() 
+    print("flashbang wrapper timed out")
+end
+
+
+if Waiter then
+    Waiter.waitFor(waitingFor, wrapCwFlashbang, onTimout )
+else
+    WaiterQueue = WaiterQueue or {}
+
+    local struct = {}
+    struct["waitingFor"] = waitingFor
+    struct["onSuccess"] =  wrapCwFlashbang
+    struct["onTimeout"] = onTimout
+
+    table.insert( WaiterQueue, struct )
+end
