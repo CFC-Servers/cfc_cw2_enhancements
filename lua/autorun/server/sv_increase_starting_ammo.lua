343041b093c5e365234051eb749ed05ec4b207f5
Brandon Sturgeon <sturgeonb4@gmail.com>
2020-09-25 00:07:48 -0400

    refactor

--- lua/autorun/server/sv_increase_starting_ammo.lua
+++ lua/autorun/server/sv_increase_starting_ammo.lua
@@ -6,12 +6,17 @@ local blacklistedAmmo = {
     -- we don't actually have to add RPG ammo here
 }
 
+local cwAmmo = {}
+
 function givePlayerCWAmmo( ply )
-    for ammoName, bulletInfo in pairs( CustomizableWeaponry.ammoTypes ) do
-        if not blacklistedAmmo[ammoName] then
-            ply:SetAmmo( 1000, ammoName )
-        end
+    for ammoName in pairs( cwAmmo ) do
+        ply:SetAmmo( 1000, ammoName )
     end
 end
 
 hook.Add( "PlayerSpawn", "CFC_CWEnhancements_GiveCW2Ammo", givePlayerCWAmmo )
+hook.Add( "Initialize", "CFC_CW2_GetAmmoTypes", function()
+    for ammoName in pairs( CustomizableWeaponry.ammoTypes ) do
+        cwAmmo[ammoName] = blacklistedAmmo[ammoName] == nil
+    end
+end )
